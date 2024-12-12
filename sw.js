2748cdb7813ff8c11f295f1ca7d517887b072423
Dustin Thomas <dustinleethomas@gmail.com>
2024-12-12 09:21:29 -0700

    remove old sw fetch handler

--- sw.js
+++ sw.js
@@ -67,27 +67,3 @@ self.addEventListener('fetch', (e) => {
         return response;
     })());
 });
-/*
-// On fetch, intercept server requests
-// and respond with cached responses instead of going to network
-self.addEventListener("fetch", (event) => {
-  // As a single page app, direct app to always go to cached home page.
-  if (event.request.mode === "navigate") {
-    event.respondWith(caches.match("/"));
-    return;
-  }
-
-  // For all other requests, go to the cache first, and then the network.
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(event.request.url);
-      if (cachedResponse) {
-        // Return the cached response if it's available.
-        return cachedResponse;
-      }
-      // If resource isn't in the cache, return a 404.
-      return new Response(null, { status: 404 });
-    })(),
-  );
-});*/
