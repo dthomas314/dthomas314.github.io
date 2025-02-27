032305c65b4c14bb882b342e85b0d6d1bd9d19f4
Dustin Thomas <dustinleethomas@gmail.com>
2025-02-27 11:31:55 -0700

    File cleanup

--- PWA/TreasureQuest/sw.js
+++ PWA/TreasureQuest/sw.js
@@ -1,5 +1,5 @@
 // The version of the cache.
-const VERSION = "2.0";
+const VERSION = "2.0.1";
 
 // The name of the cache
 const CACHE_NAME = `treasure-quest-${VERSION}`;
@@ -20,6 +20,7 @@ const APP_STATIC_RESOURCES = [
   "/PWA/TreasureQuest/assets/success.mp3",
   "/PWA/TreasureQuest/assets/Roy/helo-audio.mp3",
   "/PWA/TreasureQuest/assets/Roy/map-sketch.png",
+  "/PWA/TreasureQuest/assets/Roy/ransom.png",
   "/PWA/TreasureQuest/assets/Roy/video1.mp4"
 ];
 
